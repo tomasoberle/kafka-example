a9ec2db1f0951f14993522b88e664ea369f7dfff
tomasoberle <85870406+tomasoberle@users.noreply.github.com>
2022-06-12 14:06:53 +0200

    producer and consumer - add/use type in header + default type (consumer)

---
+++ src/main/java/com/github/tomasoberle/kafka/example/consumer/KafkaJsonTypeConsumer.java
@@ -0,0 +1,21 @@
+package com.github.tomasoberle.kafka.example.consumer;
+
+import com.github.tomasoberle.kafka.example.dto.TestDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(value = "kafka-consumer-type", havingValue = "KAFKA_LISTENER_JSON_TYPE")
+public class KafkaJsonTypeConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(KafkaJsonTypeConsumer.class);
+
+    @KafkaListener(topics = "json-topic")
+    public void listen(TestDto testDto) {
+        log.info("{}", testDto);
+    }
+
+}
